0a47cb25fa1c7cec68ca2d6c0b4a494652564621
Manfred Stock <mstock@cpan.org>
2016-04-29 21:50:49 +0200

    Use stacked bar charts to visualise Redmine issue counts
    That way, the tracker can also be displayed, which can be interesting.

--- public/app.js
+++ public/app.js
@@ -87,9 +87,20 @@
 			return {
 				restrict: 'E',
 				link:     function (scope, element, attrs) {
-					scope.chartLabels = [];
-					scope.chartData   = [[]];
-					scope.statusStats = [];
+					scope.chartLabels  = [];
+					scope.chartSeries  = [];
+					scope.chartData    = [[]];
+					scope.statusStats  = [];
+					scope.chartOptions = {
+						scales: {
+							xAxes: [{
+								stacked: true
+							}],
+							yAxes: [{
+								stacked: true
+							}]
+						}
+					};
 
 					scope.$watch(function () {
 						return statusService.getServiceStatus(scope.statusId)
@@ -100,25 +111,58 @@
 						console.log("Old: ", oldValue, "New: ", newValue);
 
 						var statusStats = {};
+						var trackers = {};
 						newValue.issues.forEach(function (issue) {
 							if (!statusStats[issue.status.id]) {
 								statusStats[issue.status.id] = {
 									id: issue.status.id,
 									name: issue.status.name,
-									count: 0
+									count: 0,
+									tracker: {}
 								};
 								scope.statusStats.push(statusStats[issue.status.id]);
 							}
+							if (!statusStats[issue.status.id].tracker[issue.tracker.id]) {
+								statusStats[issue.status.id].tracker[issue.tracker.id] = {
+									id: issue.tracker.id,
+									name: issue.tracker.name,
+									count: 0
+								}
+							}
 							statusStats[issue.status.id].count++;
+							statusStats[issue.status.id].tracker[issue.tracker.id].count++;
+							if (!trackers[issue.tracker.id]) {
+								trackers[issue.tracker.id] = {
+									name: issue.tracker.name,
+									id: issue.tracker.id
+								};
+							}
 						});
-
+						var trackerList = [];
+						Object.keys(trackers).forEach(function (trackerId) {
+							trackerList.push(trackers[trackerId]);
+						});
+						trackerList.sort(function (a, b) {
+							return a.name.localeCompare(b.name);
+						});
+						scope.chartSeries = trackerList.map(function (tracker) {
+							return tracker.name;
+						});
 						scope.chartLabels.length = 0;
 						scope.chartData[0].length = 0;
 						scope.statusStats.forEach(function (statusType) {
 							scope.chartLabels.push(statusType.name);
-							scope.chartData[0].push(statusType.count);
+							trackerList.forEach(function (tracker, index) {
+								if (!scope.chartData[index]) {
+									scope.chartData[index] = [];
+								}
+								scope.chartData[index].push(
+									statusType.tracker[tracker.id]
+										? statusType.tracker[tracker.id].count
+										: 0
+								);
+							})
 						});
-
 						scope.issueCount = newValue.issues.length;
 					});
 				},
