c79104fc19a91a101078165a5a6e07f4e382237f
Manfred Stock <mstock@cpan.org>
2016-11-03 21:46:54 +0100

    Add directive to create an Icinga2 service status grid
    Useful to see which services on which hosts are in trouble.

--- share/public/plugins/icinga2.js
+++ share/public/plugins/icinga2.js
@@ -1,11 +1,12 @@
 (function () {
 	'use strict';
 
+	var HOST_STATUSES = ['UP', 'DOWN', 'UNREACHABLE'];
+	var SERVICE_STATUSES = ['OK', 'WARNING', 'CRITICAL', 'UNKNOWN'];
+
 	angular.module('StatusDashboard').directive('icinga2', [
 		'statusService',
 		function (statusService) {
-			var HOST_STATUSES = ['UP', 'DOWN', 'UNREACHABLE'];
-			var SERVICE_STATUSES = ['OK', 'WARNING', 'CRITICAL', 'UNKNOWN'];
 			return {
 				restrict:    'E',
 				link:        function (scope, element, attrs) {
@@ -56,6 +57,52 @@
 				scope:       {
 					statusId:    '@statusId',
 					statusTitle: '@statusTitle'
+				}
+			}
+		}
+	]).directive('icinga2ServiceGrid', [
+		'statusService',
+		function (statusService) {
+			return {
+				restrict:    'E',
+				link:        function (scope, element, attrs) {
+					scope.$watch(function () {
+						return statusService.getStatus(scope.statusId)
+					}, function (newValue, oldValue) {
+						if (newValue === null) {
+							return;
+						}
+						var problemServices = {};
+						var problemHostServiceStates = {};
+						newValue.services.forEach(function (service) {
+							var serviceStatus = SERVICE_STATUSES[service.service_state];
+							if (serviceStatus !== 'OK' && !(scope.hideAcknowledged && service.service_acknowledged === 1)) {
+								var serviceId =
+									service.service_display_name || service.service_description;
+								problemServices[serviceId] = true;
+								if (!problemHostServiceStates[service.host_name]) {
+									problemHostServiceStates[service.host_name] = {};
+								}
+								problemHostServiceStates[service.host_name][serviceId] = {
+									status: serviceStatus,
+									acknowledged: service.service_acknowledged === 1
+								};
+							}
+						});
+						scope.problemServices = Object.keys(problemServices).sort();
+						scope.problemHosts = Object.keys(problemHostServiceStates).sort();
+						scope.problemHostServiceStates = problemHostServiceStates;
+					});
+					scope.$watch('hideAcknowledgedStr', function (newValue, oldValue) {
+						scope.hideAcknowledged = scope.$eval(newValue);
+					});
+				},
+				replace:     false,
+				templateUrl: 'plugins/templates/icinga-service-grid.html',
+				scope:       {
+					statusId:            '@statusId',
+					statusTitle:         '@statusTitle',
+					hideAcknowledgedStr: '@hideAcknowledged'
 				}
 			}
 		}
